605f058e25c53d3ff1f62331f078046d6b617fd8
Ken Wilcox <wilcoxk@gmail.com>
2015-08-02 21:04:27 -0600

    dealing with API Keys

---
+++ Swift/Scriv/APIKeys.template.swift
@@ -0,0 +1,11 @@
+//
+//  APIKeys.swift
+//  Scriv
+//
+//  Created by Kenneth Wilcox on 8/2/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import Foundation
+
+let DropboxAppKey = "--get your own--"
